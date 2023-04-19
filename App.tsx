a77d3ff9d20bf8d625f13d95dd6ee0ffd1b734f9
MuhammadFaisal215 <77613789+MuhammadFaisal215@users.noreply.github.com>
2023-04-19 12:51:03 +0500

    background services and push notifications

--- App.tsx
+++ App.tsx
@@ -16,6 +16,63 @@ import HomeContainer from './src/containers/Home.container';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './src/stores/reducers';
+// import PushNotification from 'react-native-push-notification';
+import messaging from '@react-native-firebase/messaging';
+import firebase from 'react-native-firebase';
+// PushNotification.configure({
+//   onRegister: function(token:any) {
+//     console.log('TOKEN:', token);
+//   },
+//   onNotification: function(notification:any) {
+//     console.log('NOTIFICATION:', notification);
+//   },
+//   permissions: {
+//     alert: true,
+//     badge: true,
+//     sound: true,
+//   },
+//   popInitialNotification: true,
+//   requestPermissions: true,
+// });
+
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyB9vMh3BH3oEd_gU4t6tuGwNNKqkqqQBKQ",
+  databaseURL: "https://fitness2well.firebaseio.com",
+  projectId: "fitness2well",
+  storageBucket: "fitness2well.appspot.com",
+  messagingSenderId: "515841385068",
+  appId: "1:515841385068:web:212de99c0f86debc4dae49"
+}, 'Test App');
+
+// Request user permission for receiving notifications
+messaging().requestPermission();
+
+// Get the device token
+messaging()
+  .getToken()
+  .then(token => {
+    console.log(token);
+  });
+
+// Receive notifications
+messaging().onMessage(notification => {
+  console.log(notification);
+});
+
+// Handle background notifications
+messaging().setBackgroundMessageHandler(async notification => {
+  console.log(notification);
+  // Do something with the notification
+});
+
+// Handle foreground notifications
+messaging().onNotificationOpenedApp(notification => {
+  console.log(notification);
+  // Do something with the notification
+});
+
 
 function App(): JSX.Element {
   return (
--- src/stores/actions/todoActions.ts
+++ src/stores/actions/todoActions.ts
@@ -2,8 +2,15 @@ import { addItem, removeItem, toggleCheck } from "../reducers/todoReducer"
 import { ThunkAction, ThunkDispatch } from 'redux-thunk';
 import { AnyAction } from '@reduxjs/toolkit';
 import {store} from '../reducers'
+import {AppRegistry} from 'react-native';
+
+
+const headlessTask = () => {
+    console.log('headlessTask is performinig')
+}
 
 export const createTodo = (value: string): ThunkAction<Promise<void>, typeof store, null, AnyAction> => async (dispatch: ThunkDispatch<typeof store, null, AnyAction>): Promise<void> => {
+    AppRegistry.registerHeadlessTask('headlessTaskKey', ():any => headlessTask())
     dispatch(addItem(value))
 }
 
